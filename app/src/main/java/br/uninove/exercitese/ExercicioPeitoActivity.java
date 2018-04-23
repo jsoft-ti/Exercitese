eaf2dd2606d5cd47406b8f8787013bd6ebc662c6
João Carlos <jsoft-ti@hotmail.com>
2018-04-23 19:10:32 -0300

    Exercitese

---
+++ app/src/main/java/br/uninove/exercitese/ExercicioPeitoActivity.java
@@ -0,0 +1,13 @@
+package br.uninove.exercitese;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ExercicioPeitoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercicio_peito);
+    }
+}
